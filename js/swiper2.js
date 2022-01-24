e1306d9cc94370e68aba6eeb4353e89e2d7f018d
Mary Hsieh <a5925457@gmail.com>
2022-01-24 17:00:20 +0800

    mobile menu

---
+++ js/swiper2.js
@@ -0,0 +1,4 @@
+var swiper2 = new Swiper(".swiper2", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+});
---
+++ js/swiper3.js
@@ -0,0 +1,4 @@
+var swiper3 = new Swiper(".swiper3", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+});
