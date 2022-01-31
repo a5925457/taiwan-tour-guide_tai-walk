e0074d054c302e2c66b8bdab3d2e6722b4046f61
Mary Hsieh <a5925457@gmail.com>
2022-01-31 16:54:32 +0800

    fetch homepage data

--- js/bigSwiper.js
+++ js/bigSwiper.js
@@ -1,15 +1,15 @@
 // Init Swiper
-var swiper = new Swiper(".bigSwiper", {
-  // Install Plugin To Swiper
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  // autoplay
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  // Enable debugger
-  debugger: true,
+var swiper = new Swiper('.bigSwiper', {
+    // Install Plugin To Swiper
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // autoplay
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // Enable debugger
+    debugger: true,
 });
