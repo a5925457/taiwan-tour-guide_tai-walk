70694079379a25c1e44e317bb08b0e8c915cb631
Mary Hsieh <a5925457@gmail.com>
2022-02-04 18:36:22 +0800

    version 1.2

--- js/swiper2.js
+++ js/swiper2.js
@@ -1,17 +1,15 @@
 var swiper2 = new Swiper('.swiper2', {
     slidesPerView: 'auto',
     spaceBetween: 16,
+    observer: true,
+    observeParents: true,
 
     // Install Plugin To Swiper
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-    // autoplay
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+
     // Enable debugger
     debugger: true,
 });
--- js/swiper3.js
+++ js/swiper3.js
@@ -1,17 +1,15 @@
 var swiper3 = new Swiper('.swiper3', {
     slidesPerView: 'auto',
     spaceBetween: 16,
+    observer: true,
+    observeParents: true,
 
     // Install Plugin To Swiper
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-    // autoplay
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+
     // Enable debugger
     debugger: true,
 });
